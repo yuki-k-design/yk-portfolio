d9f970209c286ae9891d7d6eff99a9ec12ab6468
yuki-k-design <yuki.k.design@gmail.com>
2020-09-01 23:21:01 +0900

    変更をdeploy

---
+++ public/js/script.js
@@ -0,0 +1,56 @@
+jQuery(document).ready(function () {
+
+	// スムーズスクロール
+	$(function(){
+		$('a[href^="#"]').click(function() {
+			var speed = 400; // スクロール速度をミリ秒で記述
+			var href= $(this).attr("href");
+			var target = $(href == "#" || href == "" ? 'html' : href);
+			var position = target.offset().top;
+			$('body,html').animate({scrollTop:position}, speed, 'swing');
+			return false;
+		});
+	});
+
+	// 途中からヘッダーを表示
+	$(function(){
+		var _window = $(window),
+			scrollHeaderPc = $('.scroll-header-pc'),
+			topviewBottom;
+		_window.on('scroll',function(){
+				topviewBottom = $('header').height() + $('.top-view').height();
+				if(_window.scrollTop() > topviewBottom){
+					scrollHeaderPc.addClass('show');
+				}
+				else{
+					scrollHeaderPc.removeClass('show');
+				}
+		});
+		_window.trigger('scroll');
+	});
+
+	// 途中からmenuの文字色を変更
+	$(function(){
+		var _window = $(window),
+			menu = $('.sidenav_toggle'),
+			messageBottom,
+			skillsBottom;
+		_window.on('scroll',function(){
+				messageBottom = $('header').height() + $('.top-view').height() + $('#message').height() + 100;
+				skillsBottom = messageBottom + $('#skills').height() + 250;
+				if(skillsBottom > _window.scrollTop() && _window.scrollTop() > messageBottom){
+					menu.addClass('change-menu-color');
+				}
+				else{
+					menu.removeClass('change-menu-color');
+				}
+		});
+		_window.trigger('scroll');
+	});
+
+	// menuの開閉
+  $('.sidenav_toggle').on('click', function () {
+		$('.sidenav_toggle, .sidenav').toggleClass('open');
+	});
+
+});
